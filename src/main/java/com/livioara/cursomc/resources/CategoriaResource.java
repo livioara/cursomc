9c2140ad58cfcf2ad918332045c09ac0163a01d0
Livio Ara <assislivio@gmail.coom>
2022-04-12 05:35:10 -0300

    Primeiro Commit

--- src/main/java/com/livioara/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/livioara/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,14 @@
 package com.livioara.cursomc.resources;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.livioara.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -11,8 +17,17 @@ public class CategoriaResource {
 	
 		
 		@RequestMapping(method = RequestMethod.GET)
-		public String listar() {
-		return "REST está funcinando";
+		public List<Categoria> listar() {
+			
+			Categoria cat1 = new Categoria(1, "Informatica");
+			Categoria cat2 = new Categoria(2, "Informatica");
+			
+			List<Categoria> lista = new ArrayList<>();
+			lista.add(cat1);
+			lista.add(cat2);
+			
+			
+		return lista;
 	}
 
 }
